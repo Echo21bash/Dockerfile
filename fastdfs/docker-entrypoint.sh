937138e9f0dce1b202deb1480c3eb295a4a55c36
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-06-02 03:22:52 +0800

    Update docker-entrypoint.sh

--- fastdfs/docker-entrypoint.sh
+++ fastdfs/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 [[ x${TRACKER_ADDR2} != x ]] && sed -i "s%#tracker_server=0.0.0.0:22123%tracker_server=${TRACKER_ADDR2}%" ${FDFS_HOME}/etc/client.conf ${FDFS_HOME}/etc/storage.conf /etc/fdfs/mod_fastdfs.conf
 [[ x${TRACKER_ADDR3} != x ]] && sed -i "s%#tracker_server=0.0.0.0:22124%tracker_server=${TRACKER_ADDR3}%" ${FDFS_HOME}/etc/client.conf ${FDFS_HOME}/etc/storage.conf /etc/fdfs/mod_fastdfs.conf
 [[ x${TRACKER_PORT} != x ]] && sed -i "s%port=22122%port=${TRACKER_PORT}%" ${FDFS_HOME}/etc/tracker.conf
-[[ x${STORAGE_PORT} != x ]] && sed -i "s%port=23000%port=${STORAGE_PORT}%" ${FDFS_HOME}/etc/storage.conf
+[[ x${STORAGE_PORT} != x ]] && sed -i "s%port=23000%port=${STORAGE_PORT}%" ${FDFS_HOME}/etc/storage.conf && sed -i "s/storage_server_port=23000/storage_server_port=$STORAGE_PORT/" /etc/fdfs/mod_fastdfs.conf
 [[ x${HTTP_PORT} != x ]] && sed -i "s%listen.*%listen    ${HTTP_PORT};%" ${NGINX_HOME}/conf/nginx.conf
 [[ "$SERVER_TYPE" =~ "tracker" || "$SERVER_TYPE" = "" ]] && ${FDFS_HOME}/bin/fdfs_trackerd ${FDFS_HOME}/etc/tracker.conf
 [[ "$SERVER_TYPE" =~ "storage" || "$SERVER_TYPE" = "" ]] && ${FDFS_HOME}/bin/fdfs_storaged ${FDFS_HOME}/etc/storage.conf && /opt/nginx/sbin/nginx
