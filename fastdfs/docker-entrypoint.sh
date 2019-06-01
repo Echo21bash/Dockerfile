32da3f8e9030f7b17bd28863a4534a6833e71332
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-06-02 02:06:12 +0800

    Update docker-entrypoint.sh

--- fastdfs/docker-entrypoint.sh
+++ fastdfs/docker-entrypoint.sh
@@ -5,6 +5,5 @@
 [[ x${TRACKER_PORT} != x ]] && sed -i "s%port=22122%port=${TRACKER_PORT}%" ${FDFS_HOME}/etc/tracker.conf
 [[ x${STORAGE_PORT} != x ]] && sed -i "s%port=23000%port=${STORAGE_PORT}%" ${FDFS_HOME}/etc/storage.conf
 [[ "$SERVER_TYPE" =~ "tracker" || "$SERVER_TYPE" = "" ]] && ${FDFS_HOME}/bin/fdfs_trackerd ${FDFS_HOME}/etc/tracker.conf
-[[ "$SERVER_TYPE" =~ "storage" || "$SERVER_TYPE" = "" ]] && ${FDFS_HOME}/bin/fdfs_storaged ${FDFS_HOME}/etc/storage.conf
-/opt/nginx/sbin/nginx
+[[ "$SERVER_TYPE" =~ "storage" || "$SERVER_TYPE" = "" ]] && ${FDFS_HOME}/bin/fdfs_storaged ${FDFS_HOME}/etc/storage.conf && /opt/nginx/sbin/nginx
 tail -f /dev/null
