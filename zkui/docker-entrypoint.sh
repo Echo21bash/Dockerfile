09c1f200ff01c9653d19ab0474ad2272daa29e11
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-06-24 16:14:58 +0800

    Update docker-entrypoint.sh

--- zkui/docker-entrypoint.sh
+++ zkui/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 ZK_SERVER=${ZK_SERVER:-"localhost:2181"}
 
 USER_SET=${USER_SET:-"{\"users\": [{ \"username\":\"admin\" , \"password\":\"manager\",\"role\": \"ADMIN\" \},{ \"username\":\"appconfig\" , \"password\":\"appconfig\",\"role\": \"USER\" \}]\}"}
+
 LOGIN_MESSAGE=${LOGIN_MESSAGE:-"Please login using admin/manager or appconfig/appconfig."}
 
 sed -i "s%^zkServer=.*%zkServer=$ZK_SERVER%" /opt/jar/config.cfg
