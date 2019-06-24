722bdc52779603182df11f959de85b4cb344d130
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-06-24 16:11:32 +0800

    Update docker-entrypoint.sh

--- zkui/docker-entrypoint.sh
+++ zkui/docker-entrypoint.sh
@@ -5,9 +5,9 @@ ZK_SERVER=${ZK_SERVER:-"localhost:2181"}
 USER_SET=${USER_SET:-"{\"users\": [{ \"username\":\"admin\" , \"password\":\"manager\",\"role\": \"ADMIN\" \},{ \"username\":\"appconfig\" , \"password\":\"appconfig\",\"role\": \"USER\" \}]\}"}
 LOGIN_MESSAGE=${LOGIN_MESSAGE:-"Please login using admin/manager or appconfig/appconfig."}
 
-sed -i "s/^zkServer=.*$/zkServer=$ZK_SERVER/" /opt/jar/config.cfg
-sed -i "s/^userSet = .*$/userSet = $USER_SET/" /opt/jar/config.cfg
-sed -i "s/^loginMessage=.*$/loginMessage=$LOGIN_MESSAGE/" /opt/jar/config.cfg
+sed -i "s%^zkServer=.*%zkServer=$ZK_SERVER%" /opt/jar/config.cfg
+sed -i "s%^userSet = .*%userSet = $USER_SET%" /opt/jar/config.cfg
+sed -i "s%^loginMessage=.*%loginMessage=$LOGIN_MESSAGE%" /opt/jar/config.cfg
 
 echo "Starting zkui with server $ZK_SERVER"
 
