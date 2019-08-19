98e7c010095619cc7076d25df748a6b45372211d
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-08-19 16:54:16 +0800

    Update docker-entrypoint.sh

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 #conf redis
-sed -i 's/^port 6379/port '${redis_port}'/' /opt/redis/etc/redis.conf
 sed -i "s/# requirepass foobared/requirepass ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
 sed -i 's/appendonly no/appendonly yes/' /opt/redis/etc/redis.conf
-
+if [ $REDIS_RUN_MODE = cluster ];then
+  sed -i "s/^# masterauth <master-password>/masterauth ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
+  sed -i 's/# cluster-enabled yes/cluster-enabled yes/' /opt/redis/etc/redis.conf
+  sed -i 's/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-6379.conf/' /opt/redis/etc/redis.conf
+  sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/' /opt/redis/etc/redis.conf
+fi
 #start redis
 /opt/redis/bin/redis-server /opt/redis/etc/redis.conf
+
