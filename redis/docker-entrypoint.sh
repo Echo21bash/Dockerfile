095f5b7f1ed88ba2548eaeccd203bfec0472fff7
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-08-19 16:19:03 +0800

    Update docker-entrypoint.sh

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+#conf redis
+sed -i 's/^port 6379/port '${redis_port}'/' /opt/redis/etc/redis.conf
+sed -i "s/# requirepass foobared/requirepass ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
+sed -i 's/appendonly no/appendonly yes/' /opt/redis/etc/redis.conf
+
+#start redis
+/opt/redis/bin/redis-server /opt/redis/etc/redis.conf
