a4401cea1985979480a0415966ea0678a0747725
hebaodan <hebaodan21@qq.com>
2020-08-06 12:27:08 +0800

    updata

--- redis/alpine/docker-entrypoint.sh
+++ redis/alpine/docker-entrypoint.sh
@@ -7,12 +7,12 @@ MAX_MEMORY=${MAX_MEMORY:-100mb}
 #conf redis
 sed -i "s/^bind.*/bind 0.0.0.0/" /opt/redis/etc/redis.conf
 sed -i 's#^dir ./#dir /opt/redis/data#' /opt/redis/etc/redis.conf
-sed -i "s/# requirepass foobared/requirepass ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
+[[ -n ${REDIS_PASSWORD} ]] && sed -i "s/# requirepass foobared/requirepass ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
 sed -i "s/# maxmemory <bytes>/maxmemory ${MAX_MEMORY}/" /opt/redis/etc/redis.conf
 sed -i "s/appendonly no/appendonly yes/" /opt/redis/etc/redis.conf
 
 if [ ${REDIS_RUN_MODE} = cluster ];then
-	sed -i "s/^# masterauth <master-password>/masterauth ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
+	[[ -n ${REDIS_MASTER_PASSWORD} ]] && sed -i "s/^# masterauth <master-password>/masterauth ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
 	sed -i 's/# cluster-enabled yes/cluster-enabled yes/' /opt/redis/etc/redis.conf
 	sed -i 's/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-6379.conf/' /opt/redis/etc/redis.conf
 	sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/' /opt/redis/etc/redis.conf
