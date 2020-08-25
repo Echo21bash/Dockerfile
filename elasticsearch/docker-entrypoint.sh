f8ed97f014b2b29b6a4338bb9a7c931e5062a24d
hebaodan <hebaodan21@qq.com>
2020-08-25 17:01:12 +0800

    updata

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 ES_CLUSTER_NAME=${ES_CLUSTER_NAME:-}
 ES_CLUSTER_HOSTS=${ES_CLUSTER_HOSTS:-}
 ES_NODE_NAME=${ES_NODE_NAME:-}
-ES_JAVA_OPTS=${ES_JAVA_OPTS:--Xms512m -Xmx512m}
+ES_JAVA_OPTS=${ES_JAVA_OPTS:--Xms1G -Xmx1G}
 ES_OTHER_OPTS=${ES_OTHER_OPTS:-}
 run_env(){
 	useradd elasticsearch
@@ -17,6 +17,8 @@ config_set(){
 	[[ -n ${ES_NODE_NAME} ]] && sed -i "s/#node.name:.*/node.name: ${ES_NODE_NAME}/" /opt/elasticsearch/config/elasticsearch.yml
 	[[ -n ${ES_CLUSTER_NAME} ]] && sed -i "s/#cluster.name:.*/cluster.name: ${ES_CLUSTER_NAME}/" /opt/elasticsearch/config/elasticsearch.yml
 	[[ -n ${ES_CLUSTER_HOSTS} ]] && sed -i "s/#discovery.zen.ping.unicast.hosts:.*/discovery.zen.ping.unicast.hosts: ${ES_CLUSTER_HOSTS}/" /opt/elasticsearch/config/elasticsearch.yml
+	
+	sed -i "/#discovery.zen.minimum_master_nodes:.*/adiscovery.zen.ping_timeout: 120s" /opt/elasticsearch/config/elasticsearch.yml
 	sed -i "s/#network.host:.*/network.host: 0.0.0.0/" /opt/elasticsearch/config/elasticsearch.yml
 
 }
