6dae77da13e793057e6a2fbdb23a30d510068ee0
hebaodan <hebaodan21@qq.com>
2020-09-02 16:02:27 +0800

    updata

--- redis/README.md
+++ redis/README.md
@@ -8,4 +8,5 @@
     REDIS_OTHER_OPTS=
 ```
 * 其中REDIS_OTHER_OPTS设置为redis-server其他可用参数
+  示例REDIS_OTHER_OPTS="--cluster-announce-ip 192.168.1.2 --cluster-announce-port 6379 --cluster-announce-bus-port 16379"
 * 健康检测脚本health-check
--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,29 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 REDIS_RUN_MODE=${REDIS_RUN_MODE:-standard}
 REDIS_PASSWORD=${REDIS_PASSWORD:-}
 REDIS_MASTER_PASSWORD=${REDIS_PASSWORD}
 REDIS_OTHER_OPTS=${REDIS_OTHER_OPTS:-}
 MAX_MEMORY=${MAX_MEMORY:-100mb}
-#conf redis
-sed -i "s/^bind.*/bind 0.0.0.0/" /opt/redis/etc/redis.conf
-sed -i 's#^dir ./#dir /opt/redis/data#' /opt/redis/etc/redis.conf
-[[ -n ${REDIS_PASSWORD} ]] && sed -i "s/# requirepass foobared/requirepass ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
-sed -i "s/# maxmemory <bytes>/maxmemory ${MAX_MEMORY}/" /opt/redis/etc/redis.conf
-sed -i "s/appendonly no/appendonly yes/" /opt/redis/etc/redis.conf
 
-if [ ${REDIS_RUN_MODE} = cluster ];then
-	[[ -n ${REDIS_MASTER_PASSWORD} ]] && sed -i "s/^# masterauth <master-password>/masterauth ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
-	sed -i 's/# cluster-enabled yes/cluster-enabled yes/' /opt/redis/etc/redis.conf
-	sed -i 's/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-6379.conf/' /opt/redis/etc/redis.conf
-	sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/' /opt/redis/etc/redis.conf
-fi
-#updata redis cluster pod-ip
-if [[ -f /opt/redis/data/nodes-6379.conf ]];then
-	if [[ -z ${POD_IP} ]];then
-		POD_IP=$(ip addr | grep -E 'eth[0-9a-z]{1,3}|eno[0-9a-z]{1,3}|ens[0-9a-z]{1,3}|enp[0-9a-z]{1,3}' | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v  "^255\.|\.255$|^127\.|^0\." | head -n 1)
+redis_conf(){
+
+	#conf redis
+	sed -i 's/^bind.*/bind 0.0.0.0/' /opt/redis/etc/redis.conf
+	sed -i 's#^dir ./#dir /opt/redis/data#' /opt/redis/etc/redis.conf
+	sed -i "s/# maxmemory <bytes>/maxmemory ${MAX_MEMORY}/" /opt/redis/etc/redis.conf
+	sed -i 's/appendonly no/appendonly yes/' /opt/redis/etc/redis.conf
+	if [[ -n ${REDIS_PASSWORD} ]];then
+		sed -i "s/# requirepass foobared/requirepass ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
 	fi
-	sed -i -e "/myself/ s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/${POD_IP}/" /opt/redis/data/nodes-6379.conf
-fi
-#start redis
+	
+	if [[ ${REDIS_RUN_MODE} = 'cluster' ]];then
+		if [[ -n ${REDIS_MASTER_PASSWORD} ]];then
+			sed -i "s/^# masterauth <master-password>/masterauth ${REDIS_PASSWORD}/" /opt/redis/etc/redis.conf
+		fi
+		sed -i 's/# cluster-enabled yes/cluster-enabled yes/' /opt/redis/etc/redis.conf
+		sed -i 's/# cluster-config-file nodes-6379.conf/cluster-config-file nodes-6379.conf/' /opt/redis/etc/redis.conf
+		sed -i 's/# cluster-node-timeout 15000/cluster-node-timeout 15000/' /opt/redis/etc/redis.conf
+	fi
+}
+
+redis_conf
 exec "$@" ${REDIS_OTHER_OPTS}
 
