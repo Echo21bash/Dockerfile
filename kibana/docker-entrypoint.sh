3198081dcc91107ef8b82460aeea91b2a8d7734a
hebaodan <hebaodan21@qq.com>
2020-11-27 15:59:39 +0800

    updata

--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -8,6 +8,7 @@ ES_PASSWORD=${ES_PASSWORD:-}
 config_set(){
 
 	sed -i "s/#server.host:.*/server.host: \"0\"/" /opt/kibana/config/kibana.yml
+	sed -i "s/#i18n.locale:.*/i18n.locale: \"zh-CN\"/" /opt/kibana/config/kibana.yml
 	[[ -n ${ES_URL} ]] && sed -i "s!#elasticsearch.hosts:.*!elasticsearch.hosts: [${ES_URL}]!" /opt/kibana/config/kibana.yml
 	[[ -n ${ES_USERNAME} ]] && sed -i "s/#elasticsearch.username:.*/elasticsearch.username: ${ES_USERNAME}/" /opt/kibana/config/kibana.yml
 	[[ -n ${ES_PASSWORD} ]] && sed -i "s/#elasticsearch.password:.*/elasticsearch.password: ${ES_PASSWORD}/" /opt/kibana/config/kibana.yml
