c24cc36287bc861c18e8912d54bfbc718a44744e
hebaodan <hebaodan21@qq.com>
2021-06-02 17:49:56 +0800

    logstash jvm配置

--- logstash/docker-entrypoint.sh
+++ logstash/docker-entrypoint.sh
@@ -6,10 +6,13 @@ LOGSTASH_CONF_EXTRA_DIR="${LOGSTASH_BASE_DIR}/config.d"
 LOGSTASH_BIN_DIR="${LOGSTASH_BASE_DIR}/bin"
 LOGSTASH_LOG_DIR="${LOGSTASH_BASE_DIR}/logs"
 LOGSTASH_CONF_FILENAME="${LOGSTASH_CONF_FILENAME:-logstash.conf}"
-JAVA_JVM_MEM="1G"
+JAVA_JVM_MEM="${JAVA_JVM_MEM:-1G}"
+LOGSTASH_OTHER_OPTS="${LOGSTASH_OTHER_OPTS:-}"
 
 logstash_set(){
     sed -i "s%# path.config.*%path.config: ${LOGSTASH_BASE_DIR}/config.d%" ${LOGSTASH_CONF_DIR}/logstash.yml
+	sed -i "s/-Xms.*/-Xms${JAVA_JVM_MEM}/" ${LOGSTASH_CONF_DIR}/jvm.options
+	sed -i "s/-Xmx.*/-Xmx${JAVA_JVM_MEM}/" ${LOGSTASH_CONF_DIR}/jvm.options
     cat > "${LOGSTASH_CONF_EXTRA_DIR}/${LOGSTASH_CONF_FILENAME}" <<EOF
 input {
   http { port => 8080 }
